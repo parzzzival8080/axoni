5ccc089c5f58640c2eb9820b921f6e74ef00dbbe
Ronald Dale Fuentebella <72976699+ronaldxdale09@users.noreply.github.com>
2025-04-26 20:55:23 +0800

    .

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-
+import React from 'react';
 const LoginForm = () => {
   const [activeTab, setActiveTab] = useState('email');
   const [credentials, setCredentials] = useState({
