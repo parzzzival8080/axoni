f178dc166ad8d9374ea9dea856bf23e399129059
jaezar <tech@broadheader.com>
2025-07-04 03:28:10 +0800

    :bug:

--- src/components/common/ChatBubble.jsx
+++ src/components/common/ChatBubble.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 import { LiveChatWidget } from "@livechat/widget-react";
 
 const ChatBubble = () => {
   return (
-    <LiveChatWidget
-      license="19221915"
-      onNewEvent={(event) => {
-        console.log('LiveChat Event:', event.type, event);
-      }}
-    />
+    <div style={{ backgroundColor: "transparent" }}>
+      <LiveChatWidget
+        license="19221915"
+        onNewEvent={(event) => {
+          console.log("LiveChat Event:", event.type, event);
+        }}
+      />
+    </div>
   );
 };
 
